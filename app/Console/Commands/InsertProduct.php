14f60fecacb98998b993e2c39e56f076ad404ace
biancacsanches <biancaschs.dev@gmail.com>
2021-03-30 11:35:53 -0300

    Models, Command, FormRequest & Docker Issues

---
+++ app/Console/Commands/InsertProduct.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Repositories\ProductRepository;
+
+
+class InsertProduct extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'product:create {name} {reference} {price} {delivery_days}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct(ProductRepository $productRepository)
+    {
+        $this->productRepository = $productRepository;
+
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->productRepository->store([
+            "product" => $this->argument('name'),
+            "reference" => $this->argument('reference'),
+            "price" => $this->argument('price'),
+            "delivery_days" => $this->argument('delivery_days')
+        ]);
+    }
+}
